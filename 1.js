dc0de212017218cb684829b2926b4e5adfae4153
suneetha-kantamneni <suneetha_kantamneni@srmap.edu.in>
2024-10-05 15:42:04 +0530

    my first commit

---
+++ 1.js
@@ -0,0 +1,20 @@
+console.log("Hi");
+
+setTimeout(() => {
+    console.log("Geek");
+}, 2000);
+
+console.log("End");
+
+let x = 10;
+console.log(x);
+
+let a = 5;
+let b = 2;
+let z = a + b;
+console.log(z);
+
+let c = 5;
+let d = 2;
+let e = c * d;
+console.log(e);
---
+++ 2.js
@@ -0,0 +1,64 @@
+let a = 3;
+let x = (100 + 50) * a;
+console.log(x);
+
+
+let y = 10;
+y += 5;
+console.log(y);
+
+let c = 5;
+let d = 4;
+console.log(c **= d);
+
+const age = 20;
+const canVote = (age >= 18) ? "Yes" : "No";
+console.log(canVote);
+
+const age1 = 10;
+const canVote1 = (age1 >= 18) ? "Yes" : "No";
+console.log(canVote1);
+
+let text1 = "A";
+let text2 = "B";
+let result = text1 < text2;
+console.log(result);
+
+let text3 = "20";
+let text4 = "5";
+let result1 = text3 < text4;
+console.log(result1);
+
+let Text1 = "John";
+let Text2 = "Doe";
+let Text3 = Text1 + " " + Text2;
+console.log(Text3);
+
+let TEXT1 = "What a very ";
+TEXT1 += "nice day";
+console.log(TEXT1);
+
+
+let P = 5 + 5;
+let Q = "5" + 5;
+let R = "Hello" + 5;
+console.log(P,Q,R);
+
+// LOGICAL OPERATORS
+// logical add
+
+let A = 6;
+let B = 3;
+
+console.log((A < 10 && B > 1) + "<br>" + (A < 10 && B < 1));
+
+
+let A = 6;
+let B = 3;
+console.log(A < 10 && B > 1);  
+console.log(A < 10 && B < 1);  
+
+
+let X = 5;
+let Z = Math.pow(X,2);
+console.log(Z);
